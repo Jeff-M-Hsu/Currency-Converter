d2bb430cf7de9e735bfc45f25e17f1cdc1f2d978
jhsu42 <jhsu42@uwo.ca>
2017-09-08 18:56:12 -0400

    Refactored parts of RequestLive and RequestHistory to make RequestOutput

--- Currency-Converter/src/currency/RequestLive.java
+++ Currency-Converter/src/currency/RequestLive.java
@@ -1,8 +1,6 @@
 package currency;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
@@ -53,23 +51,7 @@ public final class RequestLive {
 				ErrorCodeHandler.errorHandler(rate);
 			}
 			else {
-				// parsed objects accessed by date
-				Date timeStamp = new Date((long)(rate.getLong("timestamp")*1000));
-				String formattedDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a").format(timeStamp);
-				String source = rate.getString("source");
-
-				// forms target currency quotes for parsing JSONObject
-				String[] quotes = new String[targetCurrency.length];
-
-				// prints conversion with timestamps
-				for(int i = 0; i < targetCurrency.length; i++) {
-					quotes[i] = source.concat(targetCurrency[i].toUpperCase());
-					double exchangeRate = rate.getJSONObject("quotes").getDouble(quotes[i]);
-					System.out.printf("%.2f" + " " + source + " in " 
-							+ targetCurrency[i] + " " + exchangeRate + " = "
-							+ "%.2f" + " (Date: " + formattedDate + ")",amount,amount*exchangeRate);
-					System.out.println("\n");
-				}
+				RequestOutput.print(rate, targetCurrency, amount);
 				response.close();
 			}
 
