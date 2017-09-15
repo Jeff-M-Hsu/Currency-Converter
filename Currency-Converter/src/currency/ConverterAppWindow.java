ad7ff7dc97044772b3ff6f49d9591714d1103a76
jhsu42 <jhsu42@uwo.ca>
2017-09-14 22:40:10 -0400

    Live functionality complete

--- Currency-Converter/src/currency/ConverterAppWindow.java
+++ Currency-Converter/src/currency/ConverterAppWindow.java
@@ -22,18 +22,29 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
-import java.awt.event.InputMethodListener;
-import java.awt.event.KeyListener;
-import java.awt.event.InputMethodEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.awt.Component;
 import javax.swing.Box;
+import javax.swing.ButtonGroup;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyAdapter;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class ConverterAppWindow {
 
 	private JFrame frmCurrencyConverter;
 	private JTextField txtEnterAmount;
+	private double amount;
+	private String[] target = new String[1];
+	private String mode;
 
 	/**
 	 * Launch the application.
@@ -57,7 +68,7 @@ public class ConverterAppWindow {
 	public ConverterAppWindow() {
 		initialize();
 	}
-
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -94,9 +105,9 @@ public class ConverterAppWindow {
 		});
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 268, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 1, 0, 0, 30, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 1, 0, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frmCurrencyConverter.getContentPane().setLayout(gridBagLayout);
 		
 		Component verticalStrut_1 = Box.createVerticalStrut(20);
@@ -146,15 +157,21 @@ public class ConverterAppWindow {
 		txtEnterAmount.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent arg0) {
-				if(!txtEnterAmount.getText().matches("[0-9]+")) {
+				if(!txtEnterAmount.getText().matches("([1-9]+\\.?\\d*)")) {
 					txtEnterAmount.setText("");
+				}
+				else {
+					amount = Double.parseDouble(txtEnterAmount.getText());
 				}
 			}
 		});
 		txtEnterAmount.addKeyListener(new KeyAdapter() {
 			public void keyTyped(KeyEvent arg0) {
-				if(!txtEnterAmount.getText().matches("[0-9]+")) {
+				if(!txtEnterAmount.getText().matches("([1-9]+\\.?\\d*)")) {
 					txtEnterAmount.setText("");
+				}
+				else {
+					amount = Double.parseDouble(txtEnterAmount.getText());
 				}
 			}
 		});
@@ -164,32 +181,103 @@ public class ConverterAppWindow {
 			@Override
 			public void mousePressed(MouseEvent e) {
 				try {
-					TargetCurrencyUI dialog = new TargetCurrencyUI();
-					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-					dialog.setVisible(true);
+					try {
+						OutputWindow dialog = new OutputWindow();
+						dialog.setFields(amount, target, mode);
+						dialog.request();
+						dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+						dialog.setVisible(true);
+					} catch (Exception f) {
+						f.printStackTrace();
+					}
 				} catch (Exception e1) {
 					System.out.println("Catastrophic application failure");
 				}
 			}
 		});
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Live");
+
+
+		GridBagConstraints gbc_rdbtnNewRadioButton = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnNewRadioButton.gridx = 7;
+		gbc_rdbtnNewRadioButton.gridy = 5;
+		frmCurrencyConverter.getContentPane().add(rdbtnNewRadioButton, gbc_rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Historical");
+		GridBagConstraints gbc_rdbtnNewRadioButton_1 = new GridBagConstraints();
+		gbc_rdbtnNewRadioButton_1.insets = new Insets(0, 0, 5, 5);
+		gbc_rdbtnNewRadioButton_1.gridx = 7;
+		gbc_rdbtnNewRadioButton_1.gridy = 6;
+		frmCurrencyConverter.getContentPane().add(rdbtnNewRadioButton_1, gbc_rdbtnNewRadioButton_1);
+		ButtonGroup rdbt = new ButtonGroup();
+		rdbt.add(rdbtnNewRadioButton);
+		rdbt.add(rdbtnNewRadioButton_1);
+		rdbtnNewRadioButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(rdbtnNewRadioButton.isSelected()) {
+					mode = rdbtnNewRadioButton.getText();
+				}
+			}
+		});
+		rdbtnNewRadioButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(rdbtnNewRadioButton_1.isSelected()) {
+					mode = rdbtnNewRadioButton_1.getText();
+				}
+			}
+		});
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				target[0] = comboBox.getSelectedItem().toString();
+			}
+		});
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		comboBox.setEditable(false);
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.gridx = 7;
+		gbc_comboBox.gridy = 7;
+		frmCurrencyConverter.getContentPane().add(comboBox, gbc_comboBox);
+		File input = new File("abbreviations.txt");
+		BufferedReader list;
+		try {
+			list = new BufferedReader(new FileReader(input));
+			String addItem = "";
+			while((addItem = list.readLine()) != null){
+				comboBox.addItem(addItem);
+			}
+			list.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("can't find file");
+		} catch (IOException e) {
+			System.out.println("can't read file");
+		}
+
+		
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewButton.gridx = 7;
-		gbc_btnNewButton.gridy = 5;
+		gbc_btnNewButton.gridy = 8;
 		frmCurrencyConverter.getContentPane().add(btnNewButton, gbc_btnNewButton);
 		
 		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
 		GridBagConstraints gbc_horizontalStrut_3 = new GridBagConstraints();
 		gbc_horizontalStrut_3.insets = new Insets(0, 0, 5, 5);
 		gbc_horizontalStrut_3.gridx = 8;
-		gbc_horizontalStrut_3.gridy = 6;
+		gbc_horizontalStrut_3.gridy = 9;
 		frmCurrencyConverter.getContentPane().add(horizontalStrut_3, gbc_horizontalStrut_3);
 		
 		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
 		GridBagConstraints gbc_horizontalStrut_2 = new GridBagConstraints();
 		gbc_horizontalStrut_2.insets = new Insets(0, 0, 5, 5);
 		gbc_horizontalStrut_2.gridx = 5;
-		gbc_horizontalStrut_2.gridy = 7;
+		gbc_horizontalStrut_2.gridy = 10;
 		frmCurrencyConverter.getContentPane().add(horizontalStrut_2, gbc_horizontalStrut_2);
 	}
 }
---
+++ Currency-Converter/src/currency/OutputWindow.java
@@ -0,0 +1,104 @@
+package currency;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.awt.Toolkit;
+
+@SuppressWarnings("serial")
+public class OutputWindow extends JDialog {
+	private double amount;
+	private String[] target;
+	private String mode;
+	public String output;
+
+	public void setFields(double amount, String[] target, String mode) {
+		this.amount = amount;
+		this.target = target;
+		this.mode = mode;
+	}
+
+	public void request() {
+		if("live".equalsIgnoreCase(mode)) {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			PrintStream sysout = new PrintStream(out);
+			PrintStream old = new PrintStream(System.out);
+			System.setOut(sysout);
+			RequestLive.requestLive(target,amount);
+			output = out.toString();
+			System.out.flush();
+			System.setOut(System.out);
+			System.out.println(old);
+			String[] formatOut = output.split("\\n");
+
+			JLabel lblNewLabel = new JLabel(formatOut[2]);
+			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+			gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+			gbc_lblNewLabel.gridx = 0;
+			gbc_lblNewLabel.gridy = 3;
+			getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+
+			JLabel lblNewLabel_1 = new JLabel(formatOut[3]);
+			GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+			gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+			gbc_lblNewLabel_1.gridx = 0;
+			gbc_lblNewLabel_1.gridy = 4;
+			getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+			JLabel lblNewLabel_2 = new JLabel(formatOut[4]);
+			GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+			gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+			gbc_lblNewLabel_2.gridx = 0;
+			gbc_lblNewLabel_2.gridy = 5;
+			getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public OutputWindow() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(OutputWindow.class.getResource("/resources/icon.png")));
+		setTitle("Result");
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 227);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{434, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 33, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+			GridBagConstraints gbc_buttonPane = new GridBagConstraints();
+			gbc_buttonPane.anchor = GridBagConstraints.SOUTH;
+			gbc_buttonPane.fill = GridBagConstraints.HORIZONTAL;
+			gbc_buttonPane.gridx = 0;
+			gbc_buttonPane.gridy = 8;
+			getContentPane().add(buttonPane, gbc_buttonPane);
+			JButton okButton = new JButton("OK");
+			okButton.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					dispose();
+				}
+			});
+			okButton.setActionCommand("OK");
+			buttonPane.add(okButton);
+			getRootPane().setDefaultButton(okButton);
+
+		}
+	}
+}
